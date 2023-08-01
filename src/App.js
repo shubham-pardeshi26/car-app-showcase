01a1b689d30eadc530c71e0fb7ff622fa57d8512
shubham-pardeshi26 <shubhampardeshi9322@gmail.com>
2023-08-01 09:51:09 +0530

    serving https request

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
 import "./App.css";
 
-const API_URL = "http://www.omdbapi.com?apikey=6f204d3d";
+const API_URL = "https://www.omdbapi.com?apikey=6f204d3d";
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
